56dd4d68e8c1b36fb52efe25c4d77204c29ba72b
Kyle Phelps <kphelps@starry.com>
2017-01-10 13:32:29 -0500

    Dump

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+PACKAGES=(
+    curl
+    git
+    gnome-terminal
+    neovim
+    vim
+    wget
+    zsh
+)
+
+DOTFILES_DIR=$(pwd)
+
+sudo dnf install -y --best --allowerasing "${PACKAGES[@]}"
+
+git submodule update --init --recursive
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh <(curl https://j.mp/spf13-vim3 -L)
+
+for vimfile in .vim*; do
+    ln -sf "${DOTFILES_DIR}/${vimfile}" "${HOME}/${vimfile}"
+done
+
+mkdir -p ~/.config
+
+for config in .config/*; do
+    ln -sf "${DOTFILES_DIR}/${config}" "${HOME}/.config/"
+done
+
+nvim +BundleInstall! +qall
+
+ln -sf "${DOTFILES_DIR}/background.jpg" ~/
+ln -sf "${DOTFILES_DIR}/.zshrc" ~/
+mkdir -p ~/.config/nvim
+ln -sf "${HOME}/.vimrc" ~/.config/nvim/init.vim
+
+mkdir -p ~/bin
+
+pushd "${DOTFILES_DIR}/gnome-terminal-colors-solarized"
+cp -r ../gnome-terminal-colors/colors/* colors
+./install.sh -s gruvbox-dark
+popd
+
+mkdir -p ~/sandbox ~/dev
